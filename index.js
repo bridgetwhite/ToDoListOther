7a2f741154f1e5403f0ce1be2217298f100894c6
Jeannette Washington <jeannettelovesjava@gmail.com>
2018-06-02 16:15:31 -0400

    Inital Commit Other

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 (function () {
-    angular.module('app', [])
+    angular.module('app', ['ngRoute'])
+      .config(function($routeProvider){
+        $routeProvider
+        .when ('/',{
+          templateUrl: 'welcome/welcome.html'
+        })
+        .when('/app', {
+          template: '<todo-app></todo-app>'
+        })
+        .otherwise ({
+          // redirectTo: '/'
+          template: '<h1>That Page Does Not Exsit</h1>'
+        });
+      });
 
 })();
--- task-list/task-list.controller.js
+++ task-list/task-list.controller.js
@@ -1,12 +1,13 @@
 (function(){
     angular.module('app')
-        .controller('taskController', function(){
+        .controller('taskController', function(DataFactory){
             var $ctrl = this;
-            
+
 
             $ctrl.delete=function(index){
                 $ctrl.toDoList.splice(index,1);
+                DataFactory.setData($ctrl.toDoList);
             }
-        
+
         });
 })();
